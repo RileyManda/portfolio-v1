5d110ee69acaca8741ff376f5808317d3f55919d
RileyManda <rileymanda69@gmail.com>
2023-12-07 15:51:29 +0200

    Switched resume menu item position

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,10 +21,10 @@ const Header = ({ homeRef, projectsRef, experienceRef, contactRef }) => {
           <Nav className="me-auto" style={{ maxHeight: '100px' }} navbarScroll>
             <Nav.Link onClick={() => scrollToRef(homeRef)} >Home</Nav.Link>
             <Nav.Link onClick={() => scrollToRef(projectsRef)}>Projects</Nav.Link>
+            <Nav.Link onClick={() => scrollToRef(experienceRef)}>Experience</Nav.Link>
             <Nav.Link href="https://drive.google.com/file/d/1ahl7i3U51XcgLn4KXKoHZY885YKwsIdJ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
               Resume
             </Nav.Link>
-            <Nav.Link onClick={() => scrollToRef(experienceRef)}>Experience</Nav.Link>
             <Nav.Link onClick={() => scrollToRef(contactRef)}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
