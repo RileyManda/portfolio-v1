1c376ee0a0c05dcd42a2737a5b21ac4e085de7f1
RileyManda <rileymanda69@gmail.com>
2023-12-08 22:52:50 +0200

    updated website theme color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,14 @@ const Header = ({ homeRef, projectsRef, experienceRef, contactRef }) => {
   return (
     <Navbar expand="lg" sticky="top" variant="dark" className="custom-navbar">
       <Container fluid>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="/" className="brand">
           RileyManda
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto" style={{ maxHeight: '100px' }} navbarScroll>
+          <Nav style={{ maxHeight: '100px' }} navbarScroll >
             <Nav.Link onClick={() => scrollToRef(homeRef)} >Home</Nav.Link>
-            <Nav.Link onClick={() => scrollToRef(projectsRef)}>Projects</Nav.Link>
+            <Nav.Link  onClick={() => scrollToRef(projectsRef)}>Projects</Nav.Link>
             <Nav.Link href="https://drive.google.com/file/d/1ahl7i3U51XcgLn4KXKoHZY885YKwsIdJ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
               Resume
             </Nav.Link>
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -4,19 +4,19 @@ const SocialMediaButtons = () => {
     return (
         <div className="social-media-buttons">
             <a href="https://www.linkedin.com/in/rileymanda/" target="_blank" rel="noopener noreferrer">
-                <FaLinkedin style={{ color: 'blue', width: '50px', height: '50px' }} />
+                <FaLinkedin style={{ width: '50px', height: '50px' }} />
             </a>
             <a href="https://github.com/RileyManda/" target="_blank" rel="noopener noreferrer">
-                <FaGithub style={{ color: 'blue', width: '50px', height: '50px' }} />
+                <FaGithub style={{ width: '50px', height: '50px' }} />
             </a>
             <a href="https://stackoverflow.com/users/6129553/rileymanda" target="_blank" rel="noopener noreferrer">
-                <FaStackOverflow style={{ color: 'blue', width: '50px', height: '50px' }} />
+                <FaStackOverflow style={{ width: '50px', height: '50px' }} />
             </a>
             <a href="https://medium.com/@rileymanda" target="_blank" rel="noopener noreferrer">
-                <FaMedium style={{ color: 'blue', width: '50px', height: '50px' }} />
+                <FaMedium style={{  width: '50px', height: '50px' }} />
             </a>
             <a href="https://twitter.com/rileycodez" target="_blank" rel="noopener noreferrer">
-                <FaTwitter style={{ color: 'blue', width: '50px', height: '50px' }} />
+                <FaTwitter style={{  width: '50px', height: '50px' }} />
             </a>
 
 
