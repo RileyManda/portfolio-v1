abd949dc4987263dcca23ef2bcdb207b9aaefdc8
RileyManda <rileymanda69@gmail.com>
2023-12-09 18:19:38 +0200

    Updated introduction data

---
+++ src/api/IntroductionData.jsx
@@ -0,0 +1,9 @@
+const introductionData = [
+    {
+        title: 'Hello I am Riley',
+        description: 'I am a full-stack software engineer specializing in JavaScript, React, Redux,Ruby on Rails,Flutter and Nodejs. ',
+        whatimadoing: 'I am currently building an Event Management System called Eventharbor with a potential market niche of 10,000+ Users',
+    }
+];
+
+export default introductionData;
