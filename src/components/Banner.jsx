3dead5c3138546eb15af47c23c275dc0ad90cbf9
RileyManda <rileymanda69@gmail.com>
2023-12-09 18:19:14 +0200

    Removed  redux from project

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,26 +1,17 @@
-import { useMemo, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchProjects } from '../redux/projects/projectSlice';
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 // import Carousel from 'react-bootstrap/Carousel';
-import Badge from 'react-bootstrap/Badge';
 import Stack from 'react-bootstrap/Stack';
 import { BsFillCaretDownSquareFill } from 'react-icons/bs';
-import { ProgrammingIcons, FrontendIcons, BackendIcons, EditorIcons } from './Icons';
-import { orangeIconStyles, blueIconStyles, purpleIconStyles, redIconStyles } from './IconColor';
-import introductionData from './IntroductionData';
+import introductionData from '../api/IntroductionData';
 import AnimData from './AnimData';
 import SocialMediaButtons from './SocialMedia';
 
 const Banner = () => {
-  const dispatch = useDispatch();
   const [currentAnimData] = useState(0);
 
-  useMemo(() => {
-    dispatch(fetchProjects());
-  }, [dispatch]);
 
   const handleArrowClick = () => {
     const projectsSection = document.getElementById('projects');
@@ -42,39 +33,6 @@ const Banner = () => {
                   <Stack gap={3} direction="vertical" className='stack-layout'>
                     <h1 className="greeting">{project.title}</h1>
                     <p className="intro">{project.description}</p>
-                    <p className="whatimdoing">{project.whatimadoing}</p>
-                    <Stack direction="horizontal" gap={3}>
-                      <Badge pill bg="dark">Front-end</Badge>
-                      {FrontendIcons.map((icon, index) => (
-                        <div key={index} className="p-2 white-icon">
-                          {icon.icon({ style: blueIconStyles, title: icon.label })}
-                        </div>
-                      ))}
-                    </Stack>
-                    <Stack direction="horizontal" gap={3}>
-                      <Badge pill bg="dark">Languages</Badge>
-                      {ProgrammingIcons.map((icon, index) => (
-                        <div key={index} className="p-2 white-icon">
-                          {icon.icon({ style: orangeIconStyles, title: icon.label })}
-                        </div>
-                      ))}
-                    </Stack>
-                    <Stack direction="horizontal" gap={3}>
-                      <Badge pill bg="dark">Back-end</Badge>
-                      {BackendIcons.map((icon, index) => (
-                        <div key={index} className="p-2 white-icon">
-                          {icon.icon({ style: purpleIconStyles, title: icon.label })}
-                        </div>
-                      ))}
-                    </Stack>
-                    <Stack direction="horizontal" gap={3}>
-                      <Badge pill bg="dark">IDEs</Badge>
-                      {EditorIcons.map((icon, index) => (
-                        <div key={index} className="p-2 white-icon">
-                          <icon.icon style={redIconStyles} title={icon.label} />
-                        </div>
-                      ))}
-                    </Stack>
                   </Stack>
                 </Col>
                 <Col sm={12} lg={6} className="carousel-item-content d-flex justify-content-center">
