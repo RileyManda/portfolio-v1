4246c525bef819448a870d104ac25ea5089a27aa
RileyManda <rileymanda69@gmail.com>
2023-12-10 00:08:37 +0200

    Added experience menu item to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import PropTypes from 'prop-types';
 import Nav from 'react-bootstrap/Nav';
 
-const Header = ({ homeRef, projectsRef, contactRef }) => {
+const Header = ({ homeRef, projectsRef, experienceRef, contactRef }) => {
   const scrollToRef = (ref) => {
     if (ref.current) {
       window.scrollTo({ top: ref.current.offsetTop - 100, behavior: 'smooth' });
@@ -21,6 +21,7 @@ const Header = ({ homeRef, projectsRef, contactRef }) => {
           <Nav className="" style={{ maxHeight: '100px' }} navbarScroll >
             <Nav.Link  onClick={() => scrollToRef(homeRef)} >Home</Nav.Link>
             <Nav.Link onClick={() => scrollToRef(projectsRef)}>Projects</Nav.Link>
+            <Nav.Link onClick={() => scrollToRef(experienceRef)}>Experience</Nav.Link>
             <Nav.Link href="https://drive.google.com/file/d/1ahl7i3U51XcgLn4KXKoHZY885YKwsIdJ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
               Resume
             </Nav.Link>
