8e174610a6fc9c7c6037b939d046b1957026f5e0
RileyManda <rileymanda69@gmail.com>
2023-12-09 23:46:11 +0200

    Centered experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,35 +1,35 @@
-import Container from "react-bootstrap/Container";
+import { Container } from "react-bootstrap";
 import experienceData from "../api/experienceData";
-import Stack from 'react-bootstrap/Stack'
+import Stack from 'react-bootstrap/Stack';
 
 const Experience = () => {
   return (
     <div className="experience-container">
-    <Container fluid className="py-5">
-        <div className="section-title">
-          <h2>Experience</h2>
-        </div>
+      <div className="section-title py-5 d-flex justify-content-center align-items-center">
+        <h2>Experience</h2>
+      </div>
+      <Container fluid className="py-5 d-flex justify-content-center align-items-center">
         <div className="main-timeline">
-      <ul className="timeline-with-icons">
-        {experienceData.map((experience, index) => (
-            <li className="timeline-item mb-5" key={index}>
-            <Stack direction="vertical" gap={3} className="timeline-icon">
-              {experience.icons.map((icon, i) => (
-                <div key={i} style={{ color: experience.iconColor[i] }}>
-                  {icon}
-                </div>
-              ))}
-            </Stack>
-            <h5 className="fw-bold">{experience.position}</h5>
-            <h5 className="fw-bold">{experience.company}</h5>
-            <p className="text-muted mb-2 fw-bold">{experience.date}</p>
-            <p className="text-muted">{experience.description}</p>
-            <p style={{ color: experience.iconColor }}></p>
-          </li>
-        ))}
-      </ul>
+          <ul className="timeline-with-icons">
+            {experienceData.map((experience, index) => (
+              <li className={`timeline-item mb-5 ${index % 2 === 0 ? 'left' : 'right'}`} key={index}>
+                <Stack direction="vertical" gap={3} className="timeline-icon">
+                  {experience.icons.map((icon, i) => (
+                    <div key={i} style={{ color: experience.iconColor[i] }}>
+                      {icon}
+                    </div>
+                  ))}
+                </Stack>
+                <h5 className="fw-bold">{experience.position}</h5>
+                <h5 className="fw-bold">{experience.company}</h5>
+                <p className="text-muted mb-2 fw-bold">{experience.date}</p>
+                <p className="text-muted">{experience.description}</p>
+                <p style={{ color: experience.iconColor }}></p>
+              </li>
+            ))}
+          </ul>
         </div>
-    </Container>
+      </Container>
     </div>
   );
 };
