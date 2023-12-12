14a1d08e23f44d1b43793ab51d047b2f86b6d501
RileyManda <rileymanda69@gmail.com>
2023-12-12 09:31:49 +0200

    Updated form redirection to new tab

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,84 +1,25 @@
-import { useState } from 'react';
+// ContactForm.js
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+
 
 const ContactForm = () => {
-    const [formValues, setFormValues] = useState({
-        name: '',
-        _subject: '',
-        email: '',
-        message: '',
-    });
-
-    const handleChange = (e) => {
-        setFormValues({ ...formValues, [e.target.name]: e.target.value });
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormValues({
-            name: '',
-            _subject: '',
-            email: '',
-            message: '',
-        });
-    };
-
     return (
-        <Form
-            action="https://formsubmit.co/rileymanda0@gmail.com"
-            method="POST"
-            target="_blank"
-            onSubmit={handleSubmit}
-            style={{ border: `1px solid var(--brand)`, borderRadius: '8px', padding: '20px', width: '100%' }}
-        >
+        <Form action="https://formsubmit.co/rileymanda0@gmail.com" method="POST" target="_blank"  style={{ border: `1px solid var(--brand)`, borderRadius: '8px', padding: '20px', width: '100%' }}>
             <Form.Group controlId="formName">
-                <Form.Control
-                    type="text"
-                    name="name"
-                    placeholder="Enter your name"
-                    value={formValues.name}
-                    onChange={handleChange}
-                    required
-                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
-                />
+                <Form.Control type="text" name="name" placeholder="Enter your name" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
             </Form.Group>
             <Form.Group controlId="formSubject">
-                <Form.Control
-                    type="text"
-                    name="_subject"
-                    placeholder="Enter your subject"
-                    value={formValues._subject}
-                    onChange={handleChange}
-                    required
-                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
-                />
+                <Form.Control type="text" name="_subject" placeholder="Enter your subject" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
             </Form.Group>
             <Form.Group controlId="formEmail">
-                <Form.Control
-                    name="email"
-                    type="email"
-                    placeholder="Enter your email"
-                    value={formValues.email}
-                    onChange={handleChange}
-                    required
-                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
-                />
+                <Form.Control name="email" type="email" placeholder="Enter your email" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
             </Form.Group>
             <Form.Group controlId="formMessage">
-                <Form.Control
-                    as="textarea"
-                    rows={3}
-                    placeholder="Type your message here"
-                    value={formValues.message}
-                    onChange={handleChange}
-                    required
-                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
-                />
+                <Form.Control as="textarea" name="message" rows={3} placeholder="Type your message here" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
             </Form.Group>
-            <Button className="send-button" type="submit">
+            <button className="send-button" type="submit">
                 Send
-            </Button>
+            </button>
         </Form>
     );
 };
