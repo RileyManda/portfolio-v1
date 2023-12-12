062921c3f14de42bb3b62a62e0f2ddee3709ff2f
RileyManda <rileymanda69@gmail.com>
2023-12-12 09:39:05 +0200

    Bug fix handle form submit

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,21 +1,95 @@
-// ContactForm.js
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 
-
 const ContactForm = () => {
+    const [formValues, setFormValues] = useState({
+        name: '',
+        _subject: '',
+        email: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = document.createElement('form');
+        form.action = 'https://formsubmit.co/rileymanda0@gmail.com'; 
+        form.method = 'POST';
+        form.target = '_blank';
+
+        // Attach form data
+        Object.entries(formValues).forEach(([name, value]) => {
+            const input = document.createElement('input');
+            input.type = 'hidden';
+            input.name = name;
+            input.value = value;
+            form.appendChild(input);
+        });
+
+        document.body.appendChild(form);
+        form.submit();
+
+        // Clear the form after submission
+        setFormValues({
+            name: '',
+            _subject: '',
+            email: '',
+            message: '',
+        });
+    };
+
     return (
-        <Form action="https://formsubmit.co/rileymanda0@gmail.com" method="POST" target="_blank"  style={{ border: `1px solid var(--brand)`, borderRadius: '8px', padding: '20px', width: '100%' }}>
+        <Form
+            onSubmit={handleSubmit}
+            style={{ border: `1px solid var(--brand)`, borderRadius: '8px', padding: '20px', width: '100%' }}
+        >
             <Form.Group controlId="formName">
-                <Form.Control type="text" name="name" placeholder="Enter your name" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
+                <Form.Control
+                    type="text"
+                    name="name"
+                    placeholder="Enter your name"
+                    value={formValues.name}
+                    onChange={handleChange}
+                    required
+                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
+                />
             </Form.Group>
             <Form.Group controlId="formSubject">
-                <Form.Control type="text" name="_subject" placeholder="Enter your subject" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
+                <Form.Control
+                    type="text"
+                    name="_subject"
+                    placeholder="Enter your subject"
+                    value={formValues._subject}
+                    onChange={handleChange}
+                    required
+                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
+                />
             </Form.Group>
             <Form.Group controlId="formEmail">
-                <Form.Control name="email" type="email" placeholder="Enter your email" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
+                <Form.Control
+                    name="email"
+                    type="email"
+                    placeholder="Enter your email"
+                    value={formValues.email}
+                    onChange={handleChange}
+                    required
+                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
+                />
             </Form.Group>
             <Form.Group controlId="formMessage">
-                <Form.Control as="textarea" name="message" rows={3} placeholder="Type your message here" required style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }} />
+                <Form.Control
+                    as="textarea"
+                    rows={3}
+                    placeholder="Type your message here"
+                    name="message"
+                    value={formValues.message}
+                    onChange={handleChange}
+                    required
+                    style={{ border: `1px solid var(--brand)`, borderRadius: '4px' }}
+                />
             </Form.Group>
             <button className="send-button" type="submit">
                 Send
